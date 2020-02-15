2df13dfe80e0985429e72969b6a161e23c10c02f
Kotono <tadanotorch@gmail.com>
2020-02-15 08:14:14 +0800

    Fix 0x47

--- CSYetiTools/OpCodes/ExtraDialogCode.cs
+++ CSYetiTools/OpCodes/ExtraDialogCode.cs
@@ -3,16 +3,17 @@ namespace CSYetiTools.OpCodes
     public class ExtraDialogCode : DynamicLengthStringCode
     {
         // if Short1 == -1, it's used as normal dialog
-        // else, it's character name
+        // else if Short1 == 0x0D, it's character name
+        // else it's special string
         public ExtraDialogCode() : base(0x47) { }
 
         public bool IsDialog
             => Short1 == -1;
 
         public bool IsCharacter
-            => Short1 != -1;
+            => Short1 == 0x0D;
 
         protected override string ArgsToString(bool noString)
-            => (IsCharacter ? "<Character> " : "<Dialog> ") + base.ArgsToString(noString);
+            => (IsCharacter ? "<Character> " : "<ExDialog> ") + base.ArgsToString(noString);
     }
 }
